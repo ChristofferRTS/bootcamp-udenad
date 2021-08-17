4deabf7cae4bdb1c7e413de8182930f4318beb08
ChristofferRTS <4906240@rts-365.dk>
2021-08-17 13:05:47 +0200

    add assignment 01a

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -2,6 +2,34 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	let	number = 5;
+	let count = document.getElementById('text-count');
+	let incrementor = document.getElementById('btn-count-increase');		
+	let reductor = document.getElementById('btn-count-decrease');
+	
+	function updateNumber(){
+		document.getElementById('text-count').innerHTML = number;
+	}
+
+	updateNumber();
+
+	incrementor.addEventListener('click', plusOne);
+
+	function plusOne(){
+		if(number<10){
+			number++;
+			updateNumber();
+		}
+	}
+
+
+	reductor.addEventListener('click', minusOne);
+	
+	function minusOne(){
+		if(number>0){
+			number--;
+			updateNumber();
+		}
+	}
 
 }); // Afslutter: DOMContentLoaded
