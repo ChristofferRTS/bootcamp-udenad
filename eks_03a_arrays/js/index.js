5f91e9be8cea34cd5cc21bacdaf97a3ab90d266b
ChristofferRTS <4906240@rts-365.dk>
2021-08-20 08:46:36 +0200

    add solution to assignment 03a

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -1,6 +1,11 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	let navne = ["Mickey","Anders","Fedtmule"];
+	let ulElement = document.querySelector('#liste');
+
+	navne.forEach((navn) => {
+		ulElement.innerHTML += `<li> ${navn} </li>`;
+	})
 
 }); // Afslutter: DOMContentLoaded
